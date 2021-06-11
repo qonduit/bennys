42667c324b1c371adb090e31b78484581136dbf7
qonduit <aarongarwood@hotmail.co.uk>
2021-06-11 11:57:08 +0100

    re

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import NavBar from "./NavBar.js";
+import logo from "../static/logo.jpg";
+
+const Header = () => {
+  return <div></div>;
+};
+
+export default Header;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar-container">
+      <div className="nav-bar-item">MENU</div>
+      <div className="nav-bar-item">CONTACT</div>
+    </div>
+  );
+};
+
+export default NavBar;
